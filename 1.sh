b0f8ac5a72be34335f8597cfe4ef3ec14e734e3d
Berkay <78595496+berkaybasar@users.noreply.github.com>
2024-06-02 21:53:48 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,10 @@
-sudo apt-get update -y
-sudo apt-get install golang-go -y
-wget https://raw.githubusercontent.com/berkaybasar/berkaybasar.github.io/main/onepunch.go
-go build onepunch.go
-echo "Kuruldu Scriptin Paşa :3"
-echo "./onepunch --site https://site.com/"
+#!/bin/bash
+
+# SSH yapılandırma dosyasını yedekle
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+
+# SSH yapılandırma dosyasını düzenle
+sudo sed -i 's/^Port [0-9]*$/Port 2222/' /etc/ssh/sshd_config
+
+# SSH servisini yeniden başlat
+sudo systemctl restart ssh
